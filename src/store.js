95ae3e46e68ef1d7fd312e9884678d4d494c43b2
Jared Williams <accounts@jaredwilliams.com.au>
2019-03-23 16:50:55 +0800

    Redux logger

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import keysReducer from './ducks/keys/reducer'
 import chordsReducer from './ducks/chords/reducer'
 
 export default createStore(combineReducers({
   keys: keysReducer,
   chords: chordsReducer
-}))
+}), applyMiddleware(logger))
