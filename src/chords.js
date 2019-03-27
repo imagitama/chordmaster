71119a2b423d851b0d7844a154bd292c4a79b64c
Jared Williams <accounts@jaredwilliams.com.au>
2019-03-27 16:50:06 +0800

    Add G/F# chord

--- src/chords.js
+++ src/chords.js
@@ -456,6 +456,21 @@ export const chords = [
         5: 3
       }
     }
+  },
+
+  {
+    fullName: 'G slash F#',
+    shortName: 'G/F#',
+    frets: {
+      2: {
+        5: 2,
+        6: 1
+      },
+      3: {
+        1: 4,
+        2: 3
+      }
+    }
   }
 ]
 
