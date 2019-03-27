df9d565ca5eb20b4c18480f52e925ca5c07d78ee
Jared Williams <accounts@jaredwilliams.com.au>
2019-03-27 16:49:50 +0800

    Persist store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App'
-import store from './store'
+import createStore from './store'
+
+const { store, persistor } = createStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 document.getElementById('root'))
--- src/store.js
+++ src/store.js
@@ -1,9 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import keysReducer from './ducks/keys/reducer'
 import chordsReducer from './ducks/chords/reducer'
 
-export default createStore(combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
   keys: keysReducer,
   chords: chordsReducer
-}), applyMiddleware(logger))
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(logger))
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
