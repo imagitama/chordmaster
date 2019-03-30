6dbb8d002923cb4d010090a3a904d20b55aa5487
Jared Williams <accounts@jaredwilliams.com.au>
2019-03-30 21:58:08 +0800

    Change header to column on mobile

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,15 +1,25 @@
 import styled from '@emotion/styled'
+import mediaQuery from '../../mediaQueries'
 
-export const HeaderStyled = styled.header(({ theme }) => ({
-  background: theme.headerBackgroundColor,
-  color: theme.backgroundColor,
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'space-between'
-}))
+export const HeaderStyled = styled.header`
+  background-color ${({ theme }) => theme.headerBackgroundColor};
+  color: ${({ theme }) => theme.backgroundColor};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
 
-export const HeaderSegment = styled.div({
-  padding: '1rem'
-})
+  @media (min-width: ${mediaQuery.large}) {
+    flex-direction: row;
+  }
+`
+
+export const HeaderSegment = styled.div`
+  padding: 0.25rem;
+
+  @media (min-width: ${mediaQuery.large}) {
+    padding: 1rem;
+  }
+`
 
 export default HeaderStyled
---
+++ src/mediaQueries.js
@@ -0,0 +1,3 @@
+export default {
+  large: '768px'
+}
