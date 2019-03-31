43d1fdb37dc676a07b738c4173237ccef5529c3d
Jared Williams <accounts@jaredwilliams.com.au>
2019-03-31 13:54:36 +0800

    Add sentry error tracking

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import '@fortawesome/fontawesome-free/css/all.css'
+import * as Sentry from '@sentry/browser'
 import App from './App'
 import createStore from './store'
 
 const { store, persistor } = createStore()
+
+Sentry.init({
+ dsn: 'https://f4afba1340004de7b22b7e7eccbec25c@sentry.io/1427601'
+})
 
 ReactDOM.render(
   <Provider store={store}>
