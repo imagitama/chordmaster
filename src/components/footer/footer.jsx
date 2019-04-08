53eb0e4cc5cf315fb974c57d3f8ef663f990fd5c
Jared Williams <accounts@jaredwilliams.com.au>
2019-04-08 18:04:44 +0800

    Add clear storage button to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,14 +4,17 @@ import settings from '../../settings'
 import A from '../anchor/anchor'
 
 export const Footer = () => (
-  <FooterStyled>
-    <FooterSegment>
-      Made by <A href="http://www.jaredwilliams.com.au" context="Footer">Jared Williams</A>
-    </FooterSegment>
-    <FooterSegment>
-      <A href={settings.githubRepoUrl} title="View the GitHub repo" context="Footer">GitHub</A>
-    </FooterSegment>
-  </FooterStyled>
+  <>
+    <FooterStyled>
+      <FooterSegment>
+        Made by <A href="http://www.jaredwilliams.com.au" context="Footer">Jared Williams</A>
+      </FooterSegment>
+      <FooterSegment>
+        <A href={settings.githubRepoUrl} title="View the GitHub repo" context="Footer">GitHub</A>
+      </FooterSegment>
+    </FooterStyled>
+    <button onClick={() => localStorage.clear()}>Nuke it</button>
+  </>
 )
 
 export default Footer
