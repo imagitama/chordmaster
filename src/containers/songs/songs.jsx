b76428dce2d67cd4b4665fb33adbe3187afeb81a
Jared Williams <accounts@jaredwilliams.com.au>
2019-04-11 11:40:01 +0800

    Split up songs page to popular and other keys

--- src/containers/songs/songs.jsx
+++ src/containers/songs/songs.jsx
@@ -5,6 +5,23 @@ import ContentArea from '../../components/content-area/content-area'
 import A from '../../components/anchor/anchor'
 import { getLinkToSongWithArtistAndTitle } from '../../utils'
 
+const SongList = ({ keys }) => (
+  <ul>
+    {keys.map(({ shortName }) => (
+      <li key={shortName}>
+        <strong>{shortName}</strong>
+        <ul>
+          {songsDefinition.filter(({ key }) => key === shortName).map(({ artistAndTitle }) => (
+            <li key={artistAndTitle}>
+              <A href={getLinkToSongWithArtistAndTitle(artistAndTitle)} isInternal context="Songs">{artistAndTitle}</A>
+            </li>
+          ))}
+        </ul>
+      </li>
+    ))}
+  </ul>
+)
+
 export default () => {
   return (
     <ContentArea>
@@ -13,20 +30,10 @@ export default () => {
         Please note this is a work in progress.
       </p>
       <hr />
-      <ul>
-        {keysDefinition.map(({ shortName }) => (
-          <li key={shortName}>
-            <strong>{shortName}</strong>
-            <ul>
-              {songsDefinition.filter(({ key }) => key === shortName).map(({ artistAndTitle }) => (
-                <li key={artistAndTitle}>
-                  <A href={getLinkToSongWithArtistAndTitle(artistAndTitle)} isInternal context="Songs">{artistAndTitle}</A>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <h2>Popular Keys</h2>
+      <SongList keys={keysDefinition.filter(({ isCommonKey }) => isCommonKey === true)} />
+      <h2>Other Keys</h2>
+      <SongList keys={keysDefinition.filter(({ isCommonKey }) => isCommonKey !== true)} />      
     </ContentArea>
   )
 }
